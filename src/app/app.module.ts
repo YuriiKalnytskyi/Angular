48ad985d4a010de225e8073675c8120e4ac668b9
YuriiKalnytskyi <yra6920@gmail.com>
2021-06-04 00:06:28 +0300

    ls3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { HomeComponent } from './components/home/home.component';
+import {RouterModule, Routes} from "@angular/router";
+import { AboutComponent } from './components/about/about.component';
+
+let  routes: Routes =[
+  {path:"" , component:HomeComponent},
+  {path:"about" ,component:AboutComponent},
+  {path:"users", loadChildren:()=>import('./modules/users/users.module').then(m=>m.UsersModule)},
+  {path:"posts", loadChildren:()=>import('./modules/posrs/posrs.module').then(m=>m.PosrsModule)}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
